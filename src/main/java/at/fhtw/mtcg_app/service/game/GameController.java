b4879b6759cdb78da2c915ee6c9429baa1c0050d
Marcel Wartecker <if21b035@technikum-wien.at>
2022-12-14 19:55:06 +0100

    getStats and getScoreboard

--- src/main/java/at/fhtw/mtcg_app/service/game/GameController.java
+++ src/main/java/at/fhtw/mtcg_app/service/game/GameController.java
@@ -1,6 +1,127 @@
 package at.fhtw.mtcg_app.service.game;
 
+import at.fhtw.dataAccessLayer.UnitOfWork;
+import at.fhtw.dataAccessLayer.repositories.CardsRepo;
+import at.fhtw.dataAccessLayer.repositories.GameRepo;
+import at.fhtw.dataAccessLayer.repositories.TradingRepo;
+import at.fhtw.dataAccessLayer.repositories.UserRepo;
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.server.Request;
+import at.fhtw.httpserver.server.Response;
 import at.fhtw.mtcg_app.controller.Controller;
+import at.fhtw.mtcg_app.model.Stats;
+import at.fhtw.mtcg_app.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController extends Controller {
+
+    private GameRepo gameRepo;
+    private UserRepo userRepo;
+
+    public GameController(GameRepo gameRepo, UserRepo userRepo){
+        this.gameRepo=gameRepo;
+        this.userRepo=userRepo;
+    }
+    public Response getUserStats(Request request) {
+
+        UnitOfWork newUnit = new UnitOfWork();
+        String username = request.getTokenUser();
+
+        if(username.equals("")){
+            return new Response(
+                    HttpStatus.UNAUTHORIZED,
+                    ContentType.PLAIN_TEXT,
+                    "Authentication information is missing or invalid"
+            );
+        }
+        try {
+            boolean userExists = this.userRepo.checkUserExists(username, newUnit);
+            if (!userExists) {
+                return new Response(
+                        HttpStatus.NOT_FOUND,
+                        ContentType.PLAIN_TEXT,
+                        "User can not be found"
+                );
+            }
+
+            Stats stats = this.userRepo.getUserStats(username, newUnit);
+            String userStatsJSON = this.getObjectMapper().writeValueAsString(stats);
+
+            return new Response(
+                    HttpStatus.OK,
+                    ContentType.JSON,
+                    userStatsJSON
+            );
+
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return new Response(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ContentType.JSON,
+                "{ \"message\" : \"Internal Server Error\" }"
+        );
+    }
+
+    public Response getScoreboard(Request request) {
+        UnitOfWork newUnit = new UnitOfWork();
+        String username = request.getTokenUser();
+
+        if(username.equals("")){
+            return new Response(
+                    HttpStatus.UNAUTHORIZED,
+                    ContentType.PLAIN_TEXT,
+                    "Authentication information is missing or invalid"
+            );
+        }
+        try {
+            boolean userExists = this.userRepo.checkUserExists(username, newUnit);
+            if (!userExists) {
+                return new Response(
+                        HttpStatus.NOT_FOUND,
+                        ContentType.PLAIN_TEXT,
+                        "User can not be found"
+                );
+            }
+
+            List<Stats> scoreboardList = new ArrayList<>();
+            scoreboardList=this.userRepo.getScoreboard(username, newUnit);
+            String scoreboard = this.getObjectMapper().writeValueAsString(scoreboardList);
+
+            return new Response(
+                    HttpStatus.OK,
+                    ContentType.JSON,
+                    scoreboard
+            );
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return new Response(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ContentType.JSON,
+                "{ \"message\" : \"Internal Server Error\" }"
+        );
+    }
+
+
+    public Response battle(String firstPlayer, String secondPlayer) {
+        System.out.println(firstPlayer +" "+ secondPlayer);
+
+
+        return new Response(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ContentType.JSON,
+                "{ \"message\" : \"Internal Server Error\" }"
+        );
+    }
 }
--- src/main/java/at/fhtw/mtcg_app/service/game/GameService.java
+++ src/main/java/at/fhtw/mtcg_app/service/game/GameService.java
@@ -1,12 +1,59 @@
 package at.fhtw.mtcg_app.service.game;
 
+import at.fhtw.dataAccessLayer.repositories.CardsRepo;
+import at.fhtw.dataAccessLayer.repositories.GameRepo;
+import at.fhtw.dataAccessLayer.repositories.TradingRepo;
+import at.fhtw.dataAccessLayer.repositories.UserRepo;
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.http.Method;
 import at.fhtw.httpserver.server.Request;
 import at.fhtw.httpserver.server.Response;
 import at.fhtw.httpserver.server.Service;
+import at.fhtw.mtcg_app.model.User;
+import at.fhtw.mtcg_app.service.trading.TradeController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameService implements Service {
+
+    private final GameController gameController;
+    private List<String> players=new ArrayList<>();
+
+    public GameService(){
+        this.gameController= new GameController(new GameRepo(), new UserRepo());
+    }
+
+
     @Override
     public Response handleRequest(Request request) {
-        return null;
+        if(request.getMethod() == Method.GET && request.getPathParts().get(0).equals("stats")){
+            return this.gameController.getUserStats(request);
+        }else if(request.getMethod() == Method.GET && request.getPathParts().get(0).equals("scoreboard")){
+            return this.gameController.getScoreboard(request);
+        }else if(request.getMethod() == Method.POST && request.getPathParts().get(0).equals("battles")){
+            String player =request.getTokenUser();
+            players.add(player);
+
+            do{
+                try{
+                    Thread.sleep(3000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }while (players.size()<2);
+            String player1=players.get(0), player2=players.get(1);
+            for(int i=0; i<2; i++){
+                players.remove(0);
+            }
+            return this.gameController.battle(player1, player2);
+        }
+        return new Response(
+                HttpStatus.BAD_REQUEST,
+                ContentType.JSON,
+                "[]"
+        );
     }
+
 }
